c210e16baea715a298bb1ac549ad2995a88ddf3c
James Cassidy <james.cassidy@gmail.com>
2020-05-14 16:00:55 -0700

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,5 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
-
-  tags = {
-    Name = "main"
-  }
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.33.0"
+  # insert the 12 required variables here
 }
